022897ab1080d1fb32cb91f7f9a461c8054bda4c
Mark Cruz <markaycruz@gmail.com>
2022-01-11 23:51:50 -0500

    update

--- server.js
+++ server.js
@@ -49,6 +49,4 @@ app.post('/login', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+app.listen(PORT);
